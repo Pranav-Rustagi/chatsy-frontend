0d5f347dc81652607f86e493b5ed77c8a169d39f
Pranav-Rustagi <pranav001100@gmail.com>
2025-03-15 22:28:54 +0530

    Added onboarding page

---
+++ src/constants/avatars.ts
@@ -0,0 +1,52 @@
+const avatars: Array<any> = [
+    {
+        id: "avatar1",
+        url: "/avatars/avatar1.jpg"
+    },
+    {
+        id: "avatar2",
+        url: "/avatars/avatar2.jpg"
+    },
+    {
+        id: "avatar3",
+        url: "/avatars/avatar3.jpg"
+    },
+    {
+        id: "avatar4",
+        url: "/avatars/avatar4.jpg"
+    },
+    {
+        id: "avatar5",
+        url: "/avatars/avatar5.jpg"
+    },
+    {
+        id: "avatar6",
+        url: "/avatars/avatar6.jpg"
+    },
+    {
+        id: "avatar7",
+        url: "/avatars/avatar7.jpg"
+    },
+    {
+        id: "avatar8",
+        url: "/avatars/avatar8.jpg"
+    },
+    {
+        id: "avatar9",
+        url: "/avatars/avatar9.jpg"
+    },
+    {
+        id: "avatar10",
+        url: "/avatars/avatar10.jpg"
+    },
+    {
+        id: "avatar11",
+        url: "/avatars/avatar11.jpg"
+    },
+    {
+        id: "avatar12",
+        url: "/avatars/avatar12.jpg"
+    }
+]
+
+export default avatars;
