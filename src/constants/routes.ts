f920f2c6768f61938e9d26f357c955969a068c0b
Pranav-Rustagi <pranav001100@gmail.com>
2025-08-17 16:10:15 +0530

    Updated utils and constants

--- src/constants/routes.ts
+++ src/constants/routes.ts
@@ -3,8 +3,10 @@ const AUTH_ROUTE = `${BASE_ROUTE}/api/auth`;
 
 const CHECK_EXISTING_USER_ROUTE = `${AUTH_ROUTE}/check-existing-user`;
 const SAVE_NEW_USER_INFO_ROUTE = `${AUTH_ROUTE}/save-new-user-info`;
+const SEARCH_USERNAME_ROUTE = `${AUTH_ROUTE}/search-username`;
 
 export {
     CHECK_EXISTING_USER_ROUTE,
-    SAVE_NEW_USER_INFO_ROUTE
+    SAVE_NEW_USER_INFO_ROUTE,
+    SEARCH_USERNAME_ROUTE
 };
